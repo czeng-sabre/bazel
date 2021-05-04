7899740564b055d1528f47ca76b246fbd34b7201
Patrick Lepore <patrick.lepore@sabre.com>
2021-05-03 20:27:58 -0400

    Adding a check to make sure REPOSITORY_AUTHORIZATION don't get sent out when we don't mean them to

--- src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkRepositoryContext.java
+++ src/main/java/com/google/devtools/build/lib/bazel/repository/starlark/StarlarkRepositoryContext.java
@@ -1009,8 +1009,14 @@ public class StarlarkRepositoryContext
   }
 
   private static final String REPOSITORY_MIRROR = System.getenv("REPOSITORY_MIRROR");
+  private static final String REPOSITORY_AUTHORIZATION = System.getenv("REPOSITORY_AUTHORIZATION");
 
   private static String mapUrl(String urlString) {
+    if (!REPOSITORY_MIRROR.toLowerCase().contains("sabre") && REPOSITORY_AUTHORIZATION != null) {
+      throw new IllegalStateException("ERROR: REPOSITORY_MIRROR is a non-Sabre URL with "
+                                      + "credentials being set in REPOSITORY_AUTHORIZATION. "
+                                      + "Please unset REPOSITORY_AUTHORIZATION.");
+    }
     try {
       if (REPOSITORY_MIRROR == null || urlString.contains("localhost")) {
         return urlString;
